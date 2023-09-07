64cd74c5995e0ae52fbd567794092f3b9b334711
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2023-09-07 14:04:50 -0400

    Update BlinkLEDFinal2.py

--- BlinkLED/BlinkLEDCode/BlinkLEDFinal2.py
+++ BlinkLED/BlinkLEDCode/BlinkLEDFinal2.py
@@ -1,12 +1,19 @@
+#Group 11's Code for Wiringpi
+
+#imports library
 import wiringpi
 
+#Initializes GPIO pin 27
 wiringpi.wiringPiSetupGpio()
-
 wiringpi.softToneCreate(27)
+
+#Outputs a frequency of 15000 to GPIO 27
 wiringpi.softToneWrite(27,15000)
 
+#Forever loop to let know its running
 while True:
     
     print("running")
-        
+
+#Kills the output.
 wiringpi.softToneWrite(27,0)
