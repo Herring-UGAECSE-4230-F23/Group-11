62f00d05866e1d14ba9e7558bfc9c6df4d7feb78
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2023-09-07 14:07:17 -0400

    Update BlinkLEDFinal3.py

--- BlinkLED/BlinkLEDCode/BlinkLEDFinal3.py
+++ BlinkLED/BlinkLEDCode/BlinkLEDFinal3.py
@@ -1,10 +1,16 @@
+#Group 11's Pigpio code
+
+#import library
 import pigpio
 
+#initialize the pi
 pi=pigpio.pi()
 
+#sets frequency and duty cycle
 pi.set_PWM_frequency(27, 100000)
 pi.set_PWM_dutycycle(27, 15)
 
+#Forever loop to keep wave running
 while True:
     
     print("running")
