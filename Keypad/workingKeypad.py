7eab4ba782881ca6dd6a35379cfa196c84afc0c2
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2023-09-14 12:14:31 -0400

    Update workingKeypad.py

--- Keypad/workingKeypad.py
+++ Keypad/workingKeypad.py
@@ -1,14 +1,21 @@
-import RPi.GPIO as GPIO
+# Group 11's Keypad
+#
+# Created 6SEP2023
+# Blessed with the mentorship of Dr. Herring
+# Property of Samuel, Isaac, Austin, and Daniel
 
+# Imports
+import RPi.GPIO as GPIO
 from time import sleep
 
+#Allows for cleaner runtime
 GPIO.setwarnings(False)
-
 GPIO.setmode(GPIO.BCM)
 
-# From Pi Pad 4, 17, 27, 22
-# 23, 24, 25, 5
+# Outputs are pins 4,  17, 27, 22
+# Inputs are pins  23, 24, 25, 5
 
+'''Setup in the Input Pins'''
 GPIO.setup(23, GPIO.IN)
 state = GPIO.input(23)
 
@@ -21,6 +28,7 @@ state = GPIO.input(25)
 GPIO.setup(5, GPIO.IN)
 state = GPIO.input(5)
 
+'''Setup the Output Pins and Start them at low'''
 GPIO.setup(4, GPIO.OUT, initial=GPIO.LOW) 
 GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW) 
 GPIO.setup(27, GPIO.OUT, initial=GPIO.LOW) 
@@ -29,20 +37,26 @@ GPIO.setup(22, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(6, GPIO.OUT, initial=GPIO.LOW)
 
-#pull up?
-one = [1, 2, 3, 'A']
-two = [4, 5, 6, 'B']
+'''Initialize the Variables'''
+# Choices of the Keypad
+one   = [1, 2, 3, 'A']
+two   = [4, 5, 6, 'B']
 three = [7, 8, 9, 'C']
-four = ['*', '0', '#', 'D']
+four  = ['*', '0', '#', 'D']
 
+# Variable to stop loop
 stop = 0
 
+'''Function Called to Read a High GPIO Input from the Pi'''
 def readKeyPad(rowNum, char):
     
+    # The variable for the users Y Value
     curVal = 0
     
+    # Sends a High to the Keypad to check for a High input in return
     GPIO.output(rowNum, GPIO.HIGH)
     
+    # Deciphers what Y Input is received and returns value to user
     if GPIO.input(23) == 1:
         curVal = char[0]
     if GPIO.input(24) == 1:
@@ -51,13 +65,16 @@ def readKeyPad(rowNum, char):
         curVal = char[2]
     if GPIO.input(5) == 1:
         curVal = char[3]
-        
+    
+    # Kills the High input
     GPIO.output(rowNum, GPIO.LOW)
     
     return curVal
 
+'''Loop to Interact with the User'''
 while stop == 0:
     
+    # Continuously calls the function with differing X Values until something is returned
     stop = readKeyPad(4, one)
     if stop != 0:
         break
@@ -70,9 +87,11 @@ while stop == 0:
     stop = readKeyPad(22, four)
     if stop != 0:
         break
-    
+ 
+# Prints the chosen value
 print("You want to print", stop)
 GPIO.output(12, GPIO.HIGH)
 GPIO.output(6, GPIO.HIGH)
 
-#GPIO.cleanup()
+
+GPIO.cleanup()
