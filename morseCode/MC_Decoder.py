23377bf068b8eb0daa6161811d74a1513fea21e3
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2023-11-02 16:15:32 -0400

    Update MC_Decoder.py

--- morseCode/MC_Decoder.py
+++ morseCode/MC_Decoder.py
@@ -1,6 +1,12 @@
 #Imports
 import RPi.GPIO as GPIO
 from time import sleep
+import time
+
+morseInput = 12
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(morseInput, GPIO.IN)
 
 #List, dictionary, or class for convertng letters into their corresponding morse code.
 lettertomorse_code_dict ={'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..'}
@@ -26,15 +32,103 @@ def morse_to_text(text):
     return ' '.join(morse_code)
 
 
-#Function that monitors the GPIO pin connected to the telegraph key and returns the duration of the press
+''' Finds the Length of a Morse Code Unit '''
+# Author: Sam Brewster
+def findLength () :
+    
+    loop = True
+    fallingEdge = 0
+    
+    while (loop == True) :
+        
+        if (GPIO.input(morseInput) == 1 and fallingEdge == 0) :
+            
+            startTime = time.time()
+            
+            while (GPIO.input(morseInput) == 1 and fallingEdge == 0) :
+            
+                if (GPIO.input(morseInput) == 0) :
+                    
+                    print(fallingEdge)
+                    fallingEdge += 1
+                    
+                    sleep(.1)
+                
+        while (GPIO.input(morseInput) == 0) :
+            
+            while (GPIO.input(morseInput) == 1 and fallingEdge != 0) :
+            
+                if (GPIO.input(morseInput) == 0) :
+                    
+                    print(fallingEdge)
+                    fallingEdge += 1
+                    sleep(.1)
+            
+        if (fallingEdge >= 5) :
+            
+            loop = False
+            endTime = time.time()
+    
+    return (endTime - startTime)/9
 
 
+''' Function that monitors the GPIO pin connected to the telegraph key and returns the duration of the press '''
+# Author: Sam Brewster
+def findDuration() :
+    
+    noPress   = True
+    loop      = True
+    timeStart = 0
+    timeStop  = 0
+    
+    while (loop == True) :
+        
+        if (GPIO.input(morseInput) == 1 and noPress == True) :
+        
+            noPress = False
+            timeStart = time.time()
+            #print(timeStart)
+        elif (GPIO.input(morseInput) == 0 and noPress == False) :
+            
+            loop = False
+            timeStop = time.time()
+            
+        sleep(.1)
+    
+    print(timeStop - timeStart)
+    
+    return timeStop
+
+
+''' Converts User Input into Dots and Dashes '''
+# Author: Daniel Corvaci
+def printDotDashSpace(durationUserGave, oneUnitLength) :
+    
+    if (int(durationUserGave) == int(oneUnitLength)) :
+        
+        print(".")
+
+'''
+def danielFunc() :
+    
+    start_time = 0
+    end_time = 0
+    duration = 0
+    
+    while True:
+        
+        GPIO.WAIT(morseInput, GPIO.FALLING)
+        start_time = time.time()
+        GPIO.WAIT(morseInput, GPIO.RISING)
+        end_time = time.time()
+        duration = end_time - start_time
+        print(duration)
+'''     
 #Function that take lenght of key press as an input and returns its corresponding unit (dot, dash, or space)
 
 
 #Function to determine the average dot and dash lenght by having the user sign attention
 # This function determines the length of a unit because the user will input -.-.-, which will give us 
-def determineLength () :
     
     # This function waits for the user to input -.-.- on the keyboard.
     # This will calculate the average length of a 'unit' by timing
@@ -46,3 +140,10 @@ def determineLength () :
 
 
 #Parts 10, 11, and 12
+    
+while True:
+    #danielFunc()
+    #findDuration()
+    print(findLength())
+    #print(GPIO.input(morseInput))
+    #sleep(.01)
