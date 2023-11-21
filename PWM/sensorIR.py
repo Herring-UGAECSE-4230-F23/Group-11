7a0ff6af02bcf0753bbd333648c456548687a03f
Samuel Peter Brewster <83558689+PlywoodMonkey@users.noreply.github.com>
2023-11-20 23:21:57 -0500

    Update sensorIR.py

--- PWM/sensorIR.py
+++ PWM/sensorIR.py
@@ -23,7 +23,7 @@ GPIO.setmode(GPIO.BCM)  # Set the naming method for GPIO
 GPIO.setup(octoPin,  GPIO.OUT, initial=GPIO.LOW)  # Setup the Output pin 12
 
 pwm=GPIO.PWM(octoPin,1000) # Create a PWM object with the given frequency on pin 12
-pwm.ChangeFrequency(1000) # Change the Frequency of the PWM object
+pwm.ChangeFrequency(20) # Change the Frequency of the PWM object
 
 # GPIO board and pin setup for the rotary encoder and switch
 GPIO.setmode(GPIO.BCM)
@@ -51,17 +51,19 @@ def turnRotaryEncoder():
     endTime = 0
     increment = 0
     none_time = 0
-    dutyCycle = 10
+    dutyCycle = 50
     rotations = 0
     startTime = 0
     theoSpeed = 0
     RPM = 0
     debounce_threshold = 1
+    frequency = 1000
+    expectedRPM = 0
 
     # Value of GPIO 24 is continuously stored in the clkLastState variable
     clkLastState = GPIO.input(clk_pin)
     pwm.start(dutyCycle)
-    pwm.ChangeFrequency(1000)
+    pwm.ChangeFrequency(frequency)
     isOn = True
     topOfTheHour = True
     startTime = time.time()
@@ -77,14 +79,16 @@ def turnRotaryEncoder():
             
             RPM = rotations
             #RPM /= elapsedTime
-            #RPM *= 5
+
             RPM *= 60
             RPM /= 3
             RPM *= dutyCycle
-            RPM /= 100
+            RPM /= 10
             rotations = 0
             
-            print("Duty Cycle: ", dutyCycle, "RPM: ", RPM)
+            expectedRPM = dutyCycle * 20 * 5         
+            
+            print("Duty Cycle: ", dutyCycle, "expectedRPM: ", expectedRPM, "actualRPM: ", RPM)
             
         if (GPIO.input(irPin) == 0) :
             
@@ -101,7 +105,7 @@ def turnRotaryEncoder():
             
             if (dtState != clkState) :
                     
-                dutyCycle += 2.5
+                dutyCycle += .5
                 
                 if (dutyCycle >= 100) :
                     
@@ -109,37 +113,41 @@ def turnRotaryEncoder():
                 
             else:
                 
-                dutyCycle -= 2.5
+                dutyCycle -= .5
                 
                 if (dutyCycle <= 0) :
                     
-                    dutyCycle = 2.5
+                    dutyCycle = 1
             
         clkLastState = clkState
         time.sleep(0.01)
 
-'''
-        if (swState == GPIO.LOW) :
-            
-            buttonDebouncing_counter = 0
-            
-            while swState == GPIO.LOW and buttonDebouncing_counter < debounce_threshold:
-                
-                time.sleep(.3)
-                swState = GPIO.input(sw_pin)
-                buttonDebouncing_counter += 1
 
-            if swState == GPIO.LOW and buttonDebouncing_counter >= 1:
+        if (swState == GPIO.LOW) :
                 
-                if (isOn == True) :
+            time.sleep(.3)
+            
+            if (isOn == True) :
+                
+                pwm.start(0)
+                pwm.stop
+                isOn = False
+                
+            else :
                     
-                    isOn = False
-                    pwm.stop
-                else :
-                    
-                    isOn = True
-                    pwm.start(dutyCycle)
-'''
+                isOn = True
+                pwm.ChangeFrequency(frequency)
+                pwm.start(dutyCycle)
+            
+            print(isOn)
+                
+        if (isOn == False) :
+            
+            pwm.stop
+            
+        else :
+            
+            pwm.start(dutyCycle)
           
             
 # Main Loop 
